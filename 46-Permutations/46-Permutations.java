d9e56874244a737df7c6a8c23156568c52c5f6b7
mithreash <mithreash77@gmail.com>
2026-07-09 15:09:21 +0530

    Create 46-Permutations

---
+++ 46-Permutations/46-Permutations.java
@@ -0,0 +1,23 @@
+// Last updated: 7/9/2026, 3:09:24 PM
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(res, new ArrayList<>(), nums, new boolean[nums.length]);
+        return res;
+    }
+
+    private void backtrack(List<List<Integer>> res, List<Integer> temp, int[] nums, boolean[] used) {
+        if (temp.size() == nums.length) {
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+            used[i] = true;
+            temp.add(nums[i]);
+            backtrack(res, temp, nums, used);
+            used[i] = false;
+            temp.remove(temp.size() - 1);
+        }
+    }
+}
