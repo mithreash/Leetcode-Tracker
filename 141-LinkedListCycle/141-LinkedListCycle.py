8e1a214195c24b17dd85348fca971fd35ae54ca1
mithreash <mithreash77@gmail.com>
2026-07-09 15:07:49 +0530

    Create 141-LinkedListCycle

---
+++ 141-LinkedListCycle/141-LinkedListCycle.py
@@ -0,0 +1,14 @@
+# Last updated: 7/9/2026, 3:07:51 PM
+class Solution:
+    def hasCycle(self, head):
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+
+        return False
