376592d6142896b464980ba36c699426017674f6
mithreash <mithreash77@gmail.com>
2026-07-09 15:07:37 +0530

    Create 178-RankScores

---
+++ 178-RankScores/178-RankScores.sql
@@ -0,0 +1,9 @@
+-- Last updated: 7/9/2026, 3:07:39 PM
+SELECT
+    s1.score,
+    COUNT(DISTINCT s2.score) AS `rank`
+FROM Scores s1
+JOIN Scores s2
+    ON s2.score >= s1.score
+GROUP BY s1.id, s1.score
+ORDER BY s1.score DESC;
