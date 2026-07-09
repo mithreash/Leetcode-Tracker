e7f74dcddf8dbb1395b6a13dae4ca006cdda4419
mithreash <mithreash77@gmail.com>
2026-07-09 15:09:40 +0530

    Create 36-ValidSudoku

---
+++ 36-ValidSudoku/36-ValidSudoku.java
@@ -0,0 +1,23 @@
+// Last updated: 7/9/2026, 3:09:43 PM
+import java.util.HashSet;
+
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> set = new HashSet<>();
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char num = board[i][j];
+
+                if (num != '.') {
+                    if (!set.add(num + "row" + i) ||
+                        !set.add(num + "col" + j) ||
+                        !set.add(num + "box" + i/3 + "-" + j/3))
+                        return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
