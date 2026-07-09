b2ebc092782f3d2b614bd9fbae133e7e19123b5e
mithreash <mithreash77@gmail.com>
2026-07-09 15:07:05 +0530

    Create 1182-GamePlayAnalysisIv

---
+++ 1182-GamePlayAnalysisIv/1182-GamePlayAnalysisIv.sql
@@ -0,0 +1,10 @@
+-- Last updated: 7/9/2026, 3:07:07 PM
+# Write your MySQL query statement below
+
+SELECT ROUND(SUM(login)/COUNT(DISTINCT player_id), 2) AS fraction
+FROM (
+  SELECT
+    player_id,
+    DATEDIFF(event_date, MIN(event_date) OVER(PARTITION BY player_id)) = 1 AS login
+  FROM Activity
+) AS t
