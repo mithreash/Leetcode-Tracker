a88a757edd078423c1e609a088ac31e25bea2731
mithreash <mithreash77@gmail.com>
2026-07-09 15:07:58 +0530

    Create 107-BinaryTreeLevelOrderTraversalIi

---
+++ 107-BinaryTreeLevelOrderTraversalIi/107-BinaryTreeLevelOrderTraversalIi.java
@@ -0,0 +1,22 @@
+// Last updated: 7/9/2026, 3:08:00 PM
+public class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        List<List<Integer>> wrapList = new LinkedList<List<Integer>>();
+        
+        if(root == null) return wrapList;
+        
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int levelNum = queue.size();
+            List<Integer> subList = new LinkedList<Integer>();
+            for(int i=0; i<levelNum; i++) {
+                if(queue.peek().left != null) queue.offer(queue.peek().left);
+                if(queue.peek().right != null) queue.offer(queue.peek().right);
+                subList.add(queue.poll().val);
+            }
+            wrapList.add(0, subList);
+        }
+        return wrapList;
+    }
+}
